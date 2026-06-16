8a1dd23607286d1c13be8e1d6786fdd88f0b656d
yiqingxiao <xiaoyiqing90@gmail.com>
2026-06-16 13:39:53 -0400

    Data directory created and set as ignored.

--- p1_char_parser/script_test_1.py
+++ p1_char_parser/script_test_1.py
@@ -11,11 +11,62 @@ src = '../'
 sys.path.append( os.path.abspath(os.path.join(currentdir, src)) )
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+
+from PIL import Image
+from tensorflow.keras.models import Sequential
+
+# Obtain the database mnist.It contains a training set of 60,000 images and a 
+# test set of 10,000 images, each representing digits from 0 to 9.
+from keras.datasets import mnist
+
 
 from toolbox import indexingUtils as idxUtils
 from toolbox.dataUtils import zscore_normalize_features
 from toolbox.dataUtils import minmax_normalize_features
 
 
+
+# ======================================================================= >>>>>
+#       mnist Image Save To File
+# ======================================================================= >>>>>
+
+# Define the directory in which the mnist figures are to be saved.
+img_dir = currentdir + '/char_img_data/mnist'
+
+# Obtain all the 0 to 9 image database.
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+
+digit_cnts = np.zeros(10) 
+
+# for z in range( len( X_train ) ):
+for z in range( 10 ):
+
+    # Obtain current image and its label.
+    image_z = X_train[z]
+    label_z = y_train[z]
+
+    # Increment the count of the target number.
+    digit_cnts[label_z] += 1
+
+    # Set the name of the figure file to be saved.
+    img_z_filename = str( label_z ) + '_n' + str( int( digit_cnts[ label_z ] ) ) + '.png'
+
+    # Specify the full path where the image will be saved
+    file_path_z = os.path.join( img_dir, img_z_filename )
+
+    # Convert the image to PIL format
+    img_z = Image.fromarray(image_z)
+
+    # Save the image
+    img_z.save(file_path_z)
+
+
+
+# ======================================================================= <<<<<
 
 
