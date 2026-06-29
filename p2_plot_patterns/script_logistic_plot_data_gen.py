c440884a6e2918fb0e5696184975786a25c5dc47
yiqingxiao <xiaoyiqing90@gmail.com>
2026-06-29 18:13:41 -0400

    p2: bulk script generating linear and logistic plot data are is now split into two scripts.

--- p2_plot_patterns/script_logistic_plot_data_gen.py
+++ p2_plot_patterns/script_logistic_plot_data_gen.py
@@ -1,6 +1,5 @@
 
 
-
 import os, sys
 # Make sure the directory above this current one is visible. This is to
 # provide access to some local libraries.
@@ -16,68 +15,10 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
 
-from plot_funcs import Lin3SegmConfig
-from plot_funcs import gen_Lin3SegmPlotData
 from plot_funcs import LogisticFuncConfig
 from plot_funcs import fit_logistic_2_pts
 from plot_funcs import get_logistic_plot_data
 
-
-# ======================================================================= >>>>>
-#       Linear Function Data Set Gen
-# ======================================================================= >>>>>
-
-# # The range of mid points allowed.
-# drop_mid_pt_rng = ( 0.10, 0.90 )
-# # The range of width the linear drop is allowed.
-# drop_width_rng = ( 0.02, 0.08 )
-
-# # The range of y starting value (highest point)
-# y_max_rng = ( 0.90, 0.95 )
-# # The range of y ending value (lowest point)
-# y_min_rng = ( 0.05, 0.10 )
-
-# # The slight dip in y from its highest point to the point where the main drop occurs.
-# y_pre_drop_dip_rng = ( 0.01, 0.05 )
-# # The slight dip in y from where the main drop ends to the lowest y value.
-# y_post_drop_dip_rng = ( 0.01, 0.05 )
-
-# # Define the number of random test cases.
-# n = 100
-# # Generate the randomized parameters for the three segments linear plot.
-# drop_mid_pt_arr = np.random.uniform( drop_mid_pt_rng[0], drop_mid_pt_rng[1], size = n )
-# drop_width_arr = np.random.uniform( drop_width_rng[0], drop_width_rng[1], size = n )
-# y_max_arr = np.random.uniform( y_max_rng[0], y_max_rng[1], size = n )
-# y_min_arr = np.random.uniform( y_min_rng[0], y_min_rng[1], size = n )
-# y_pre_drop_dip_arr = np.random.uniform( y_pre_drop_dip_rng[0], y_pre_drop_dip_rng[1], size = n )
-# y_post_drop_dip_arr = np.random.uniform( y_post_drop_dip_rng[0], y_post_drop_dip_rng[1], size = n )
-
-
-# for z in range(n):
-
-#     myConfig_z = Lin3SegmConfig()
-#     myConfig_z.x1 = 0
-#     myConfig_z.y1 = y_max_arr[z]
-#     myConfig_z.x2 = drop_mid_pt_arr[z] - drop_width_arr[z]/2
-#     myConfig_z.y2 = y_max_arr[z] - y_pre_drop_dip_arr[z]
-#     myConfig_z.x3 = drop_mid_pt_arr[z] + drop_width_arr[z]/2
-#     myConfig_z.y3 = y_min_arr[z] + y_post_drop_dip_arr[z]
-#     myConfig_z.x4 = 1
-#     myConfig_z.y4 = y_min_arr[z]
-#     myConfig_z.data_pt_cnt = 100
-
-#     x_arr_z, y_arr_z = gen_Lin3SegmPlotData( myConfig_z )
-
-#     plt.plot( x_arr_z, y_arr_z )
-    
-
-# plt.xlabel("x")
-# plt.ylabel("y")
-# plt.title("y = x")
-# plt.grid(True)
-# plt.show()
-
-# ======================================================================= <<<<<
 
 
 # ======================================================================= >>>>>
@@ -146,6 +87,3 @@ plt.grid(True)
 plt.show()
 
 # ======================================================================= <<<<<
-
-# For the gradually steeper drop profile.
-# y_min + (y_max - y_min) * np.exp(-k * x)
