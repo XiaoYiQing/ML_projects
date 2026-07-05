d673ee05b0a90f931f6d0eaeede1611f34262244
yiqingxiao <xiaoyiqing90@gmail.com>
2026-07-04 20:48:33 -0400

    p2: script_train_lin_vs_other now properly creates a model that differentiate linear drop profile from the two other profiles.

--- p2_plot_patterns/script_train_lin_vs_other.py
+++ p2_plot_patterns/script_train_lin_vs_other.py
@@ -32,13 +32,13 @@ from toolbox.indexingUtils import rand_samp
 load_dir = currentdir + '/data/norm_plot_data'
 load_lin_filestem = 'lin_drop_data'
 load_logis_filestem = 'logistic_drop_data'
-load_linPolyLin_filestem = 'linPolyLin_data'
+load_LPL_filestem = 'linPolyLin_data'
 load_fileext = '.npz'
 
 # Define the full file names for loading the data.
 load_lin_FFN = load_dir + '/' + load_lin_filestem + load_fileext
 load_logis_FFN = load_dir + '/' + load_logis_filestem + load_fileext
-load_linPolyLin_FFN = load_dir + '/' + load_linPolyLin_filestem + load_fileext
+load_LPL_FFN = load_dir + '/' + load_LPL_filestem + load_fileext
 
 
 # Load the linear drop data from the designated data directory.
@@ -52,8 +52,294 @@ y_logis = data_tmp["Y"]
 labels_logis = data_tmp["labels"]
 
 # Load the LinPolyLin drop data from the designated data directory.
-data_tmp = np.load( load_linPolyLin_FFN )
+data_tmp = np.load( load_LPL_FFN )
 y_LPL = data_tmp["Y"]
 labels_LPL = data_tmp["labels"]
+
+# ======================================================================= <<<<<
+
+
+# ======================================================================= >>>>>
+#       Data Partitioning
+# ======================================================================= >>>>>
+
+# The ratio (out of 1) of data to be assigned as training set (remaining goes to 
+# testing).
+tr_ratio = 0.4
+
+y_lin_cnt = len( y_lin )        # Total linear set data count.
+y_logis_cnt = len( y_logis )    # Total logistic set data count.
+y_LPL_cnt = len( y_LPL )        # Total LinPolyLin set data count.
+
+# Linear data set partition indexing generation.
+lin_tr_len = math.floor( y_lin_cnt*tr_ratio )
+lin_ts_len = y_lin_cnt - lin_tr_len
+lin_tr_idx, lin_ts_idx = rand_samp( y_lin_cnt, lin_tr_len )
+# Logistic data set partition indexing generation.
+logis_tr_len = math.floor( y_logis_cnt*tr_ratio )
+logis_ts_len = y_logis_cnt - logis_tr_len
+logis_tr_idx, logis_ts_idx = rand_samp( y_logis_cnt, logis_tr_len )
+# LPL data set partition indexing generation.
+LPL_tr_len = math.floor( y_LPL_cnt*tr_ratio )
+LPL_ts_len = y_LPL_cnt - LPL_tr_len
+LPL_tr_idx, LPL_ts_idx = rand_samp( y_LPL_cnt, LPL_tr_len )
+
+# Training and testing sets creation (linear drop).
+y_lin_tr = y_lin[ lin_tr_idx ]
+y_lin_ts = y_lin[ lin_ts_idx ]
+labels_lin_tr = labels_lin[ lin_tr_idx ]
+labels_lin_ts = labels_lin[ lin_ts_idx ]
+# Training and testing sets creation (logistic drop).
+y_logis_tr = y_logis[ logis_tr_idx ]
+y_logis_ts = y_logis[ logis_ts_idx ]
+labels_logis_tr = labels_logis[ logis_tr_idx ]
+labels_logis_ts = labels_logis[ logis_ts_idx ]
+# Training and testing sets creation (linear-polynomial-linear drop).
+y_LPL_tr = y_logis[ LPL_tr_idx ]
+y_LPL_ts = y_logis[ LPL_ts_idx ]
+labels_LPL_tr = labels_logis[ LPL_tr_idx ]
+labels_LPL_ts = labels_logis[ LPL_ts_idx ]
+
+# ======================================================================= <<<<<
+
+
+
+# ======================================================================= >>>>>
+#       Model Generation and Testing (Lin vs LPL)
+# ======================================================================= >>>>>
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Data Prep
+# ------------------------------------------------------------------ >>>>>
+
+load_from_save = False
+save_dir = currentdir + '/ML_model_deposit'
+model_fullfilename = save_dir + '/script_lin_vs_LPL_data_train.keras'
+
+# Create a complete set of linear and logistic plot data for training.
+X_tr = np.concatenate( [ y_lin_tr, y_LPL_tr ], axis=0 )
+y_tr = np.concatenate( [ labels_lin_tr, labels_LPL_tr ], axis=0 )
+# shuffle
+idx = np.random.permutation( len( X_tr ) )
+X_tr, y_tr = X_tr[idx], y_tr[idx]
+# add channel dimension (Initalize at one for greyscale).
+X_tr = X_tr[ ..., np.newaxis ]   # (N, L, 1)
+
+# ------------------------------------------------------------------ <<<<<
+
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Training
+# ------------------------------------------------------------------ >>>>>
+
+if load_from_save:
+
+    model = keras.models.load_model( model_fullfilename )
+
+else:
+
+    # --- simple 1D CNN classifier ---
+    L = X_tr.shape[1]  # Number of data points in each training case.
+    inputs = keras.Input( shape=(L, 1) )
+
+    x = layers.Conv1D(32, 5, activation="relu", padding="same")(inputs)
+    x = layers.MaxPooling1D(2)(x)
+    x = layers.Conv1D(64, 5, activation="relu", padding="same")(x)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Dense(32, activation="relu")(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+
+    model = keras.Model(inputs, outputs)
+    model.compile(optimizer="adam",
+                loss="binary_crossentropy",
+                metrics=["accuracy"])
+
+    model.fit( X_tr, y_tr, batch_size=32, epochs=20, validation_split=0.2 )
+
+    model.save( model_fullfilename )
+
+# ------------------------------------------------------------------ <<<<<
+
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Testing
+# ------------------------------------------------------------------ >>>>>
+
+# Create a complete set of linear and logistic plot data for testing.
+X_ts = np.concatenate( [ y_lin_ts, y_LPL_ts ], axis=0 )
+y_ts = np.concatenate( [ labels_lin_ts, labels_LPL_ts ], axis=0 )
+
+# add channel dimension (Initalize at one for grayscale).
+X_ts = X_ts[ ..., np.newaxis ]   # (N, L, 1)
+
+test_loss, test_accuracy = model.evaluate( X_ts, y_ts )
+# test_loss, test_accuracy = model.evaluate( X_tr_set, y_tr_set )
+print( f'Extra test accuracy: {test_accuracy:.4f}' )
+print( f'Extra test loss: {test_loss:.4f}' )
+
+# ------------------------------------------------------------------ <<<<<
+
+# ======================================================================= <<<<<
+
+
+
+# ======================================================================= >>>>>
+#       Model Generation and Testing (Logis vs LPL)
+# ======================================================================= >>>>>
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Data Prep
+# ------------------------------------------------------------------ >>>>>
+
+load_from_save = False
+save_dir = currentdir + '/ML_model_deposit'
+model_fullfilename = save_dir + '/script_logis_vs_LPL_data_train.keras'
+
+# Create a complete set of linear and logistic plot data for training.
+X_tr = np.concatenate( [ y_logis_tr, y_LPL_tr ], axis=0 )
+y_tr = np.concatenate( [ labels_logis_tr, labels_LPL_tr ], axis=0 )
+# shuffle
+idx = np.random.permutation( len( X_tr ) )
+X_tr, y_tr = X_tr[idx], y_tr[idx]
+# add channel dimension (Initalize at one for greyscale).
+X_tr = X_tr[ ..., np.newaxis ]   # (N, L, 1)
+
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Training
+# ------------------------------------------------------------------ >>>>>
+
+if load_from_save:
+
+    model = keras.models.load_model( model_fullfilename )
+
+else:
+
+    # --- simple 1D CNN classifier ---
+    L = X_tr.shape[1]  # Number of data points in each training case.
+    inputs = keras.Input( shape=(L, 1) )
+
+    x = layers.Conv1D(32, 5, activation="relu", padding="same")(inputs)
+    x = layers.MaxPooling1D(2)(x)
+    x = layers.Conv1D(64, 5, activation="relu", padding="same")(x)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Dense(32, activation="relu")(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+
+    model = keras.Model(inputs, outputs)
+    model.compile(optimizer="adam",
+                loss="binary_crossentropy",
+                metrics=["accuracy"])
+
+    model.fit( X_tr, y_tr, batch_size=32, epochs=20, validation_split=0.2 )
+
+    model.save( model_fullfilename )
+
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Testing
+# ------------------------------------------------------------------ >>>>>
+
+# Create a complete set of linear and logistic plot data for testing.
+X_ts = np.concatenate( [ y_logis_ts, y_LPL_ts ], axis=0 )
+y_ts = np.concatenate( [ labels_logis_ts, labels_LPL_ts ], axis=0 )
+
+# add channel dimension (Initalize at one for grayscale).
+X_ts = X_ts[ ..., np.newaxis ]   # (N, L, 1)
+
+test_loss, test_accuracy = model.evaluate( X_ts, y_ts )
+# test_loss, test_accuracy = model.evaluate( X_tr_set, y_tr_set )
+print( f'Extra test accuracy: {test_accuracy:.4f}' )
+print( f'Extra test loss: {test_loss:.4f}' )
+
+# ======================================================================= <<<<<
+
+
+
+# ======================================================================= >>>>>
+#       Model Generation and Testing (Lin vs LPL and Logis)
+# ======================================================================= >>>>>
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Data Prep
+# ------------------------------------------------------------------ >>>>>
+
+load_from_save = False
+save_dir = currentdir + '/ML_model_deposit'
+model_fullfilename = save_dir + '/script_lin_vs_other_data_train.keras'
+
+# Labels manipulation.
+labels_lin_tr = np.ones( len( labels_lin_tr ) )
+labels_LPL_tr = np.zeros( len( labels_LPL_tr ) )
+labels_logis_tr = np.zeros( len( labels_logis_tr ) )
+
+# Create a complete set of linear and logistic plot data for training.
+X_tr = np.concatenate( [ y_lin_tr, y_LPL_tr, y_logis_tr ], axis=0 )
+y_tr = np.concatenate( [ labels_lin_tr, labels_LPL_tr, labels_logis_tr ], axis=0 )
+# shuffle
+idx = np.random.permutation( len( X_tr ) )
+X_tr, y_tr = X_tr[idx], y_tr[idx]
+# add channel dimension (Initalize at one for greyscale).
+X_tr = X_tr[ ..., np.newaxis ]   # (N, L, 1)
+
+# ------------------------------------------------------------------ <<<<<
+
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Training
+# ------------------------------------------------------------------ >>>>>
+
+if load_from_save:
+
+    model = keras.models.load_model( model_fullfilename )
+
+else:
+
+    # --- simple 1D CNN classifier ---
+    L = X_tr.shape[1]  # Number of data points in each training case.
+    inputs = keras.Input( shape=(L, 1) )
+
+    x = layers.Conv1D(32, 5, activation="relu", padding="same")(inputs)
+    x = layers.MaxPooling1D(2)(x)
+    x = layers.Conv1D(64, 5, activation="relu", padding="same")(x)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Dense(32, activation="relu")(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+
+    model = keras.Model(inputs, outputs)
+    model.compile(optimizer="adam",
+                loss="binary_crossentropy",
+                metrics=["accuracy"])
+
+    model.fit( X_tr, y_tr, batch_size=32, epochs=20, validation_split=0.2 )
+
+    model.save( model_fullfilename )
+
+# ------------------------------------------------------------------ <<<<<
+
+
+# ------------------------------------------------------------------ >>>>>
+#       Model Testing
+# ------------------------------------------------------------------ >>>>>
+
+# Labels manipulation.
+labels_lin_ts = np.ones( len( labels_lin_ts ) )
+labels_LPL_ts = np.zeros( len( labels_LPL_ts ) )
+labels_logis_ts = np.zeros( len( labels_logis_ts ) )
+
+# Create a complete set of linear and logistic plot data for testing.
+X_ts = np.concatenate( [ y_lin_ts, y_LPL_ts, y_logis_ts ], axis=0 )
+y_ts = np.concatenate( [ labels_lin_ts, labels_LPL_ts, labels_logis_ts ], axis=0 )
+
+
+# add channel dimension (Initalize at one for grayscale).
+X_ts = X_ts[ ..., np.newaxis ]   # (N, L, 1)
+
+test_loss, test_accuracy = model.evaluate( X_ts, y_ts )
+# test_loss, test_accuracy = model.evaluate( X_tr_set, y_tr_set )
+print( f'Extra test accuracy: {test_accuracy:.4f}' )
+print( f'Extra test loss: {test_loss:.4f}' )
+
+# ------------------------------------------------------------------ <<<<<
 
 # ======================================================================= <<<<<
